c8e85a0d5b7caf3191a51ee32c8ddde391883795
nongzhsh <nongzhsh@outlook.com>
2019-07-08 21:51:21 +0800

    添加 UtilsModule

---
+++ 4.6.0/angular/src/shared/utils/utils.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    providers: [
+    ],
+    declarations: [
+    ],
+    exports: [
+    ]
+})
+export class UtilsModule { }
